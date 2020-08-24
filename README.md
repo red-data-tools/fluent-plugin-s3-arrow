fde352976f8a9a3b9fb63d7ba95c9339ded21664
kanga333 <e411z7t40w@gmail.com>
2020-08-24 15:24:52 +0900

    Initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # fluent-plugin-s3-arrow
+
+Extends the [fluent-plugin-s3](https://github.com/fluent/fluent-plugin-s3) (de)compression algorithm to enable red-arrow compression.
+
+## Installation
+
+### Requirements
+
+- Apache Arrow GLib and Apache Parquet GLib
+  - See Apache [Arrow install document](https://arrow.apache.org/install/) for details.
+- [red-arrow](https://github.com/apache/arrow/tree/master/ruby/red-arrow)
+- [red-parquet](https://github.com/apache/arrow/tree/master/ruby/red-parquet)
+
+### RubyGems
+
+```
+$ gem install fluent-plugin-s3-arrow
+```
+
+### Bundler
+
+Add following line to your Gemfile:
+
+```ruby
+gem "fluent-plugin-s3-arrow"
+```
+
+And then execute:
+
+```
+$ bundle
+```
+
+## Configuration
+
+Example of fluent-plugin-s3 configuration.
+
+```
+<match pattern>
+  @type s3
+
+  # fluent-plugin-s3 configurations ...
+
+  <format>
+    @type msgpack # This plugin currently supports only msgpack formatter.
+  </format>
+
+  store_as arrow
+  <compress>
+    schema [
+      {"name": "test_string", "type": "string"},
+      {"name": "test_uint64", "type": "uint64"}
+    ]
+  </compress>
+</match>
+```
+
+## Copyright
+
+* Copyright(c) 2020- kanga333
+* License
+  * Apache License, Version 2.0
---
+++ test/plugin/test_s3_compressor_arrow.rb
@@ -0,0 +1,58 @@
+require "helper"
+require 'msgpack'
+require "fluent/plugin/out_s3"
+require "fluent/plugin/s3_compressor_arrow"
+require "fluent/plugin/output"
+
+class S3OutputTest < Test::Unit::TestCase
+    def setup
+      Fluent::Test.setup
+    end
+  
+    CONFIG = %[
+      s3_bucket test_bucket
+      store_as arrow
+      <compress>
+        schema [
+          {"name": "test_string", "type": "string"},
+          {"name": "test_uint64", "type": "uint64"}
+        ]
+      </compress>
+    ]
+
+    def test_configure
+      d = create_driver
+      c = d.instance.instance_variable_get(:@compressor)
+      assert_equal :arrow, c.ext
+      assert_equal 'application/x-apache-arrow-file', c.content_type
+      assert c.instance_variable_get(:@arrow_schema).is_a?(Arrow::Schema)
+      assert_equal 1024, c.instance_variable_get(:@compress).arrow_chunk_size
+    end
+
+    def test_compress
+      d = create_driver
+      c = d.instance.instance_variable_get(:@compressor)
+
+      chunk = Fluent::Plugin::Buffer::MemoryChunk.new(Object.new)
+      d1 = {"test_string" => 'record1', "test_uint64" => 1}
+      d2 = {"test_string" => 'record2', "test_uint64" => 2}
+      chunk.append([d1.to_msgpack, d2.to_msgpack])
+      
+      Tempfile.create do |tmp|
+        c.compress(chunk, tmp)
+        Arrow::MemoryMappedInputStream.open(tmp.path) do |input|
+          reader = Arrow::RecordBatchFileReader.new(input)
+          reader.each do |record_batch|
+            assert_equal([d1, d2], record_batch.collect(&:to_h))
+          end
+        end
+      end
+    end
+
+    private
+
+    def create_driver(conf = CONFIG)
+      Fluent::Test::Driver::Output.new(Fluent::Plugin::S3Output) do
+      end.configure(conf)
+    end
+end
