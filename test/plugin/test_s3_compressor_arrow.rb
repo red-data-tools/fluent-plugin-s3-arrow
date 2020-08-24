af3b885b0e7e1ec3eda2bd3d19bbd5fd8fec36bd
kanga333 <e411z7t40w@gmail.com>
2020-08-24 16:01:19 +0900

    Add parquet comression test

--- test/plugin/test_s3_compressor_arrow.rb
+++ test/plugin/test_s3_compressor_arrow.rb
@@ -49,6 +49,36 @@ class S3OutputTest < Test::Unit::TestCase
       end
     end
 
+    PARQUET_CONFIG = %[
+      s3_bucket test_bucket
+      store_as arrow
+      <compress>
+        arrow_format parquet
+        schema [
+          {"name": "test_string", "type": "string"},
+          {"name": "test_uint64", "type": "uint64"}
+        ]
+      </compress>
+    ]
+
+    def test_compress_with_parquet
+      d = create_driver(conf=PARQUET_CONFIG)
+      c = d.instance.instance_variable_get(:@compressor)
+
+      chunk = Fluent::Plugin::Buffer::MemoryChunk.new(Object.new)
+      d1 = {"test_string" => 'record1', "test_uint64" => 1}
+      d2 = {"test_string" => 'record2', "test_uint64" => 2}
+      chunk.append([d1.to_msgpack, d2.to_msgpack])
+      
+      Tempfile.create do |tmp|
+        c.compress(chunk, tmp)
+        table = Arrow::Table.load(tmp.path, format: :parquet)
+        table.each_record_batch do |record_batch|
+          assert_equal([d1, d2], record_batch.collect(&:to_h))
+        end
+      end
+    end
+
     private
 
     def create_driver(conf = CONFIG)
