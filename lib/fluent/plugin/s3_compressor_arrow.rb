ca262df7b92d031cfe7242e765756862b36136fc
kanga333 <e411z7t40w@gmail.com>
2020-12-01 23:56:29 +0900

    Add schema_from configulation

--- lib/fluent/plugin/s3_compressor_arrow.rb
+++ lib/fluent/plugin/s3_compressor_arrow.rb
@@ -12,11 +12,17 @@ module Fluent::Plugin
       }
 
       config_section :arrow, multi: false do
-        config_param :schema, :array
+        config_param :schema, :array, default: nil
         config_param :format, :enum, list: [:arrow, :feather, :parquet], default: :arrow
         SUPPORTED_COMPRESSION = [:gzip, :snappy, :zstd]
         config_param :compression, :enum, list: SUPPORTED_COMPRESSION, default: nil
         config_param :chunk_size, :integer, default: nil
+        config_param :schema_from, :enum, list: [:glue], default: nil
+        config_section :glue, multi: false do
+          config_param :catalog, :string, default: nil
+          config_param :database, :string, default: "default"
+          config_param :table, :string, default: nil
+        end
       end
 
       def configure(conf)
@@ -26,7 +32,11 @@ module Fluent::Plugin
           raise Fluent::ConfigError, "#{@arrow.format} unsupported with #{@arrow.format}"
         end
 
-        @schema = Arrow::Schema.new(@arrow.schema)
+        if !(@arrow.schema.nil? ^ @arrow.schema_from.nil?)
+          raise Fluent::ConfigError, "Either schema or schema_from must be specified"
+        end
+
+        resolve_schema()
         @options = Arrow::JSONReadOptions.new
         @options.schema = @schema
         @options.unexpected_field_behavior = :ignore
@@ -50,6 +60,23 @@ module Fluent::Plugin
           chunk_size: @arrow.chunk_size,
           compression: @arrow.compression,
         )
+      end
+
+      private
+
+      def resolve_schema
+        if !@arrow.schema.nil?
+          @schema = Arrow::Schema.new(@arrow.schema)
+          return
+        end
+
+        require 'fluent-plugin-s3-arrow/catalogs'
+
+        case @schema_from
+        when :gelu
+          catalog = FluentPluginS3Arrow::Catalogs.lookup("aws_glue").new()
+          @schema = catalog.resolve_arrow_schema(:table, :database, :catalog)
+        end
       end
     end
   end
