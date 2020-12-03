d2e0c391139ec8411ab8af6b91de96abf5d19ce8
kanga333 <e411z7t40w@gmail.com>
2020-12-03 23:28:15 +0900

    Make static settings a section

--- lib/fluent/plugin/s3_compressor_arrow.rb
+++ lib/fluent/plugin/s3_compressor_arrow.rb
@@ -1,5 +1,6 @@
 require 'arrow'
 require 'parquet'
+require 'fluent-plugin-s3-arrow/schemas'
 
 module Fluent::Plugin
   class S3Output
@@ -12,12 +13,16 @@ module Fluent::Plugin
       }
 
       config_section :arrow, multi: false do
-        config_param :schema, :array, default: nil
         config_param :format, :enum, list: [:arrow, :feather, :parquet], default: :arrow
         SUPPORTED_COMPRESSION = [:gzip, :snappy, :zstd]
         config_param :compression, :enum, list: SUPPORTED_COMPRESSION, default: nil
         config_param :chunk_size, :integer, default: nil
-        config_param :schema_from, :enum, list: [:glue], default: nil
+        config_param :schema_from, :enum, list: [:static, :glue], default: :static
+
+        config_section :static, multi: false do
+          config_param :schema, :array, default: nil
+        end
+
         config_section :glue, multi: false do
           config_param :catalog, :string, default: nil
           config_param :database, :string, default: "default"
@@ -32,13 +37,8 @@ module Fluent::Plugin
           raise Fluent::ConfigError, "#{@arrow.format} unsupported with #{@arrow.format}"
         end
 
-        if !(@arrow.schema.nil? ^ @arrow.schema_from.nil?)
-          raise Fluent::ConfigError, "Either schema or schema_from must be specified"
-        end
-
-        resolve_schema
         @options = Arrow::JSONReadOptions.new
-        @options.schema = @schema
+        @options.schema = resolve_schema
         @options.unexpected_field_behavior = :ignore
       end
 
@@ -65,20 +65,15 @@ module Fluent::Plugin
       private
 
       def resolve_schema
-        if !@arrow.schema.nil?
-          @schema = Arrow::Schema.new(@arrow.schema)
-          return
-        end
-
-        require 'fluent-plugin-s3-arrow/schemas'
-
-        case @schema_from
+        case @arrow.schema_from
+        when :static
+          Arrow::Schema.new(@arrow.static.schema)
         when :glue
           glue_schema = FluentPluginS3Arrow::Schemas::AWSGlue.new(@arrow.glue.table, {
             catalog_id: @arrow.glue.catalog,
             database_name: @arrow.glue.database,
           })
-          @schema = glue_schema.to_arrow
+          glue_schema.to_arrow
         end
       end
     end
